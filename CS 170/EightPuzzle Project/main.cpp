c0a7124c3b67b4ddb6416741bb421f2ccb44235a
Juan Ruiz <j_ruiz05@yahoo.com>
2017-10-13 09:56:55 -0700

    Added functions to create, fill, and print the tiles

--- CS 170/EightPuzzle Project/main.cpp
+++ CS 170/EightPuzzle Project/main.cpp
@@ -8,8 +8,46 @@
 
 #include <iostream>
 
+
+using namespace std;
+
+int** createGrid(int); // creates the nxn grid where tiles are placed
+void fillGrid(int**, int); // the tiles from 1 to [(n x ) - 1]
+void printGrid(int**, int);
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int **grid = createGrid(3);
+    fillGrid(grid, 3);
+    printGrid(grid, 3);
     return 0;
+}
+
+
+int** createGrid(int size) {
+    int** grid = new int* [size]; // create a 2-d array
+    for (int i = 0; i != size; ++i) {
+        grid[i] = new int [size];      // initialize the rows with size cols.
+    
+    }
+    return grid;
+}
+
+
+void fillGrid(int** grid, int size) {
+    for (int i = 0; i != size; ++i) {
+        for (int j = 0; j != size; ++j) {
+            grid[i][j] = (i+j);
+        }
+    }
+
+}
+
+void printGrid(int** grid, int size) {
+    for (int i = 0; i != size; ++i) {
+        for (int j = 0; j != size; ++j) {
+            cout <<grid[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
 }
