971fd0a99cee4fa79e60950fdc71da969364ad2a
Juan Ruiz <j_ruiz05@yahoo.com>
2017-10-13 09:35:50 -0700

    Started 8-puzzle project

---
+++ CS 170/EightPuzzle Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EightPuzzle
+//
+//  Created by Juan Ruiz on 10/13/17.
+//  Copyright © 2017 Juan Ruiz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
