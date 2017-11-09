1bda7cff091af763c7bf717988e4e825af200b6f
Juan Ruiz <j_ruiz05@yahoo.com>
2017-11-09 15:34:06 -0800

    Started some prelimirary stuff for project 2. Project 2 has yet to be assigned so this may not work

---
+++ CS 170/Project2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MyMachineLearner
+//
+//  Created by Juan Ruiz on 11/4/17.
+//  Copyright © 2017 Juan Ruiz. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
